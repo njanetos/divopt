9d962d29f330acfbb3eea940e5e590aa8ef0340a
njanetos <nejanetos@gmail.com>
2015-07-12 12:36:27 -0400

    Added some more tests for nuts and boltsy things

--- src/c_tests.cpp
+++ src/c_tests.cpp
@@ -1,9 +1,44 @@
 #define CATCH_CONFIG_MAIN
+#include <armadillo>
 #include "catch.hpp"
 #include "c_rand_var_norm.h"
 #include "c_logger.h"
 
 TEST_CASE("Statistics and probability") {
+
+    SECTION("Nuts and bolts") {
+
+        c_rand_var_norm rand_var_norm(2);
+
+        double raw_data[5];
+        raw_data[0] = 1;
+        raw_data[1] = 1;
+        raw_data[2] = 1;
+        raw_data[3] = 0.5;
+        raw_data[4] = 2;
+
+        rand_var_norm.dat_to_dist(raw_data);
+        rand_var_norm.unpack();
+
+        REQUIRE(rand_var_norm.get_dim() == 2);
+
+        REQUIRE(rand_var_norm.get_dim_prob() == 5);
+
+        REQUIRE(rand_var_norm.inv_cov()(0, 0) == 1.06250);
+
+        arma::Mat<double> new_mean(2, 1);
+        new_mean(0, 0) = 0.2;
+        new_mean(1, 0) = 0.3;
+
+        rand_var_norm.mean = new_mean;
+        rand_var_norm.pack();
+        rand_var_norm.unpack();
+
+        REQUIRE(rand_var_norm.mean(0, 0) == 0.2);
+
+        REQUIRE(rand_var_norm.inv_ch()(0, 0) == 1);
+
+    }
 
     SECTION("PDF") {
 
