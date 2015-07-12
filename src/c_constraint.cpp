8434d363994a8bf4e1ddf4246572be0af6af234e
njanetos <nejanetos@gmail.com>
2015-07-12 14:21:56 -0400

    Added empty cpp class for constraints

--- src/c_constraint.cpp
+++ src/c_constraint.cpp
@@ -1,11 +1,31 @@
 #include "c_constraint.h"
 
-c_constraint::c_constraint()
-{
+c_constraint::c_constraint() {
     //ctor
 }
 
-c_constraint::~c_constraint()
-{
+c_constraint::~c_constraint() {
     //dtor
+}
+
+void c_constraint::add_inequality(c_inequality inequality) {
+
+}
+
+void c_constraint::add_inequality(std::vector<c_inequality> inequalities) {
+
+}
+
+void c_constraint::add_inequality(c_inequality inequality, size_t pos) {
+
+}
+
+bool c_constraint::is_satisfied(c_rand_var rand_var) {
+
+    return false;
+
+}
+
+void c_constraint::set_prob(double prob); {
+
 }
