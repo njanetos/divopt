b9d80a1d945373f11c131f65855060544c9a5cf5
njanetos <nejanetos@gmail.com>
2015-09-07 13:01:19 -0400

    Added server.

---
+++ src/server.cpp
@@ -0,0 +1,39 @@
+#include "server_http.hpp"
+#include "client_http.hpp"
+
+//Added for the json-example
+#define BOOST_SPIRIT_THREADSAFE
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+//Added for the default_resource example
+#include <fstream>
+#include <boost/filesystem.hpp>
+
+using namespace std;
+//Added for the json-example:
+using namespace boost::property_tree;
+
+typedef SimpleWeb::Server<SimpleWeb::HTTP> HttpServer;
+typedef SimpleWeb::Client<SimpleWeb::HTTP> HttpClient;
+
+int main() {
+    //HTTP-server at port 8080 using 4 threads
+    HttpServer server(8080, 4);
+
+    //GET-example for the path /match/[number], responds with the matched string in path (number)
+    //For instance a request GET /match/123 will receive: 123
+    server.resource["^/match/([0-9]+)$"]["GET"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
+        string number=request->path_match[1];
+        response << "HTTP/1.1 200 OK\r\nContent-Length: " << number.length() << "\r\n\r\n" << number;
+    };
+
+    thread server_thread([&server](){
+        //Start server
+        server.start();
+    });
+
+    server_thread.join();
+
+    return 0;
+}
